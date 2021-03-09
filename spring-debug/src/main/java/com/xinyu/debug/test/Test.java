7ef745df00fcb3d64e05b2fb79d4a81107952964
xinyu4 <894235025@qq.com>
2021-03-09 16:25:04 +0800

    my commit

---
+++ spring-debug/src/main/java/com/xinyu/debug/test/Test.java
@@ -0,0 +1,13 @@
+package com.xinyu.debug.test;
+
+
+/**
+ * @Description
+ * @Author xinyu4
+ * @Date 2021/3/9/0009 14:04
+ */
+public class Test {
+	public static void main(String[] args) {
+
+	}
+}
---
+++ spring-mytest/src/main/java/com/xinyu/Test.java
@@ -0,0 +1,14 @@
+package com.xinyu;
+
+
+/**
+ * @Description
+ * @Author xinyu4
+ * @Date 2021/3/9/0009 15:05
+ */
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("");
+	}
+}
